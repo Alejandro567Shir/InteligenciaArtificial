60a87d83a3d42a67e26f91c0ca723a4641f97c0b
Alejandro567Shir <jose.rodriguezangamarca@gmail.com>
2024-11-26 15:17:04 -0500

    Activdad de IA

---
+++ src/main/java/com/mycompany/inteligenciaartificial/InteligenciaArtificial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inteligenciaartificial;
+
+/**
+ *
+ * @author joser
+ */
+public class InteligenciaArtificial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
